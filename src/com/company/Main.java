9f9b9c8cf24d03440bfd9ce5fc8d9d5af67dc7c9
Kuba Wilk <kuba99p@gmail.com>
2021-06-10 09:39:05 +0200

    ...

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -11,9 +11,6 @@ public class Main {
 
     public static void main(String[] args) {
 
-
-        int x =20;
-        int[][] tab = new int[x][x];
-        new WireworldWindow("Wireworld", x);
+        new WireworldWindow("Wireworld");
     }
 }
